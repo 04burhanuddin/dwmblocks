0710d989ae813d338acab321070fe4aea6d112aa
04burhanuddin <dev.burhanuddin@gmail.com>
2023-06-19 16:47:02 +0700

    first commit

---
+++ config.h
@@ -0,0 +1,14 @@
+static const Block blocks[] = {
+  {"", "clock", 5, 0},
+  {"", "forecast", 5, 0},
+  {"", "disk"},
+  {"", "volume", 1, 10},
+  {"", "memory", 6, 1},
+  {"", "cpu", 1, 6},
+  {"", "battery", 5, 3},
+  {"", "nettraf", 1, 16},
+  {"", "internet", 5 ,4},
+};
+
+static char delim[] = "  ";
+static unsigned int delimLen = 5;
