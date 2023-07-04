be980345d9b2756009bed52e86bc93f234b9e215
04burhanuddin <dev.burhanuddin@gmail.com>
2023-07-05 00:56:16 +0700

    update colors & remove others

--- config.h
+++ config.h
@@ -1,14 +1,12 @@
 static const Block blocks[] = {
-  {"", "clock", 5, 0},
-  {"", "forecast", 5, 0},
-  {"", "disk"},
-  {"", "volume", 1, 10},
-  {"", "memory", 6, 1},
-  {"", "cpu", 1, 6},
-  {"", "battery", 5, 3},
-  {"", "nettraf", 1, 16},
-  {"", "internet", 5 ,4},
+  {"^c#A89984^", "nettraf", 1, 16},
+  {"^c#CC241D^", "cpu", 1, 6},
+  {"^c#98971A^", "memory", 6, 1},
+  {"^c#D79921^", "forecast", 5, 0},
+  {"^c#458588^", "clock", 5, 0},
+  {"^c#B16286^", "battery", 5, 3},
+  {"^c#FE8019^", "internet", 5 ,4},
 };
 
 static char delim[] = "  ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 6;
