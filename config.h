5a534c30b8460aa61c8da5b8978b3302b51c5a06
04burhanuddin <dev.burhanuddin@gmail.com>
2024-01-31 14:39:33 +0700

    update new statusbar

--- config.h
+++ config.h
@@ -1,12 +1,13 @@
 static const Block blocks[] = {
   /*Icon/colors*/  /*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"^c#A89984^", "nettraf", 1, 16},
-  {"^c#CC241D^", "cpu", 1, 6},
-  {"^c#98971A^", "memory", 6, 1},
-  {"^c#D79921^", "forecast", 5, 0},
-  {"^c#458588^", "clock", 5, 0},
-  {"^c#B16286^", "battery", 5, 3},
-  {"^c#FE8019^", "internet", 5 ,4},
+  /* {"^c#F6524F^", "sb-brightness", 5, 0},*/
+  {"^c#FFC102^", "sb-memory", 6, 1},
+  {"^c#F8520E^", "sb-forecast", 5, 0},
+  /* {"^c#9DA0ED^", "sb-clock", 5, 0},*/
+  {"^c#F255A1^", "sb-nettraf", 5, 3},
+  {"^c#27EEE0^", "sb-internet", 5 ,4},
+  {"^c#9DA0ED^", "sb-volume", 5, 0},
+  {"^c#BAFB00^", "sb-battery", 5 ,4},
 };
 
 static char delim[] = "  ";
