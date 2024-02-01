40ab90231df37ef7405e67f4290dcf5ab6423e1a
04burhanuddin <dev.burhanuddin@gmail.com>
2024-02-02 00:41:46 +0700

    reorder status information

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 static const Block blocks[] = {
   /*Icon/colors*/  /*Command*/		/*Update Interval*/	/*Update Signal*/
-  /* {"^c#F6524F^", "sb-brightness", 5, 0},*/
-  {"^c#FFC102^", "sb-memory", 6, 1},
+  /*{"^c#FFC102^", "sb-memory", 6, 1},*/
   {"^c#F8520E^", "sb-forecast", 5, 0},
   /* {"^c#9DA0ED^", "sb-clock", 5, 0},*/
   {"^c#F255A1^", "sb-nettraf", 5, 3},
   {"^c#27EEE0^", "sb-internet", 5 ,4},
   {"^c#9DA0ED^", "sb-volume", 5, 0},
+  {"^c#F6524F^", "sb-brightness", 5, 0},
   {"^c#BAFB00^", "sb-battery", 5 ,4},
 };
 
